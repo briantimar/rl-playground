c6419cf4e9cdf0be86bde3e6d4cec6580859575a
Brian Timar <timarbrian@gmail.com>
2019-07-16 16:15:46 -0400

    Policy

--- models.py
+++ models.py
@@ -1,15 +1,46 @@
 import torch
+from torch.distributions.categorical import Categorical
 
-class MLP(torch.nn.Module):
+class Policy(torch.nn.Module):
+    """ Feed-forward network that represents a stochastic policy.
+        Inputs are observations from environment.
+        The output layer defines logits, which should be fed to a softmax to obtain action
+        probabilities. """
 
-    def __init__(self, layer_sizes):
+    def __init__(self):
+        super().__init__()
+    
+    def sample_action_with_log_prob(self, input):
+        """Sample action and compute its log-probability under model and input. 
+            input: tensor of input values
+            action: (batch_size, k) tensor of actions, with k an integer in 0, ... , num_actions -1"""
+        logits = self(x)
+        probs = Categorical(logits=logits)
+        action = probs.sample()
+        logprobs = probs.log_prob(action)
+        return action, logprobs
+
+class MLP(Policy):
+
+    def __init__(self, layer_sizes, activation=torch.relu):
         super().__init__()
         self.layer_sizes = layer_sizes
+        #number of layers including input and output
+        self.num_layer = len(layer_sizes)
+        self.activation = activation
         for i in range(len(layer_sizes)-1):
-            l = torch.nn.Linear(self.layer_sizes[i], self.layer_sizes[i+1])
+            l = torch.nn.Linear(self.layer_sizes[i], self.layer_sizes[i+1])               
             self.add_module("layer_%d"%i, l)
 
     def forward(self, x):
-        for layer in self.modules():
+        for i in range(self.num_layer-1):
+            layer = getattr(self, "layer_%d"%i)
             x = layer(x)
+            if i < self.num_layer - 2 and self.activation is not None:
+                x = self.activation(x)
         return x
+
+if __name__ == '__main__':
+    mlp = MLP([2, 10, 2])
+    x = torch.randn(5,2)
+    actions, lps = mlp.sample_action_with_log_prob(x)
