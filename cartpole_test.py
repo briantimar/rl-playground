d29924f4a7893277adfd2d1c8e95bd8e443a4f75
Brian Timar <timarbrian@gmail.com>
2019-07-30 14:50:52 -0400

    policy_value_model test

--- cartpole_test.py
+++ cartpole_test.py
@@ -3,23 +3,25 @@ import gym
 import torch
 
 from rl.models import MLP, ModelStepper, HyperParams
-from rl.tools import do_vpg_training, get_sample_trajectory
+from rl.tools import do_pg_training, get_sample_trajectory
 from rl.tools import do_episode, compute_rewards_to_go
 
-policy = MLP([4,20,20,2], activation=torch.relu)
+policy = MLP([4,20,20,3], activation=torch.relu, output_critic=True)
 env = gym.make('CartPole-v0')
 max_timesteps=200
-batch_size = 16
+batch_size = 32
 num_batches = 40
 
 # states, actions, rewards, log_probs = do_episode(policy, env, max_timesteps)
 
-optimizer = torch.optim.Adam(policy.parameters(),lr=.01)
+policy_optimizer = torch.optim.Adam(policy.parameters(),lr=.01)
+critic_optimizer = torch.optim.Adam(policy.parameters(),lr=.01)
 
 # value_model_hp = HyperParams(lr=.1, layer_sizes=(4, 20, 20, 1))
 # value_modelstepper = ModelStepper(MLP, lossfn=torch.nn.MSELoss(), 
 #                                 hyperparams=value_model_hp, optimizer=torch.optim.Adam)
 
-avg_returns = do_vpg_training(policy, env, max_timesteps, 
-                            optimizer=optimizer,batch_size=batch_size, num_batches=num_batches, 
-                            baseline=None)
+avg_returns = do_pg_training(policy, env, max_timesteps, 
+                            policy_optimizer=policy_optimizer,batch_size=batch_size, num_batches=num_batches, 
+                            critic_optimizer=critic_optimizer,
+                            baseline='policy_value_model')
